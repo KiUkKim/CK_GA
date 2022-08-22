24a04fb3a08fc15b1cec2ea4aad0c3343a943361
KiUkKim <zidh1@korea.ac.kr>
2022-08-22 21:47:55 +0900

    Server Update

--- src/main/java/com/ck/reusable/springboot/security/WebSecurityConfig.java
+++ src/main/java/com/ck/reusable/springboot/security/WebSecurityConfig.java
@@ -1,33 +1,36 @@
 package com.ck.reusable.springboot.security;
 
-import com.ck.reusable.springboot.Filter.Filter1;
 import com.ck.reusable.springboot.Filter.JwtAuthenticationFilter;
 import com.ck.reusable.springboot.Filter.JwtAuthorizationFilter;
+import com.ck.reusable.springboot.domain.user.RefreshJwtRepository;
 import com.ck.reusable.springboot.domain.user.UserRepository;
-import com.ck.reusable.springboot.service.user.jwtService;
+import com.ck.reusable.springboot.web.jwt.jwtCookieUtilService;
+import com.ck.reusable.springboot.web.jwt.jwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final com.ck.reusable.springboot.service.user.jwtService jwtService;
-
-    private final CorsFilter corsFilter;
+    private final CorsConfig corsConfig;
 
     private final UserRepository userRepository;
 
+    private final jwtCookieUtilService jwtCookieUtilService;
+
+    private final RefreshJwtRepository jwtRepository;
+
     private final jwtService jwtService;
+
 
     // authenticationManager를 Bean 등록합니다.
     @Bean
@@ -48,11 +51,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용x.
                 .and()
                 .formLogin().disable()
-                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
-                .addFilter(new JwtAuthenticationFilter(jwtService)) // crossorigin(인증 필요x ) , 시큐리티 필터에 인증이 필요할 때 등록
-//                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository)) // Authorization Filter
+                .httpBasic().disable()// rest api 만을 고려하여 기본 설정은 해제.
+                        .addFilter(corsConfig.corsFilter())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtService)) // crossorigin(인증 필요x ) , 시큐리티 필터에 인증이 필요할 때 등록
+//                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository, jwtRepository, jwtService, jwtCookieUtilService)) // Authorization Filter
                 .authorizeRequests() // 요청에 대한 사용권한 체크
-                .antMatchers("/","/auth/**","/login", "/save", "/phoneCheck", "/emailValidate")////이 링크들은
+                .antMatchers("/","/auth/**","/login", "/save", "/emailValidate", "/phoneCheck")////이 링크들은
                 .permitAll()///허용한다
                 .antMatchers("/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
