2550b568e0c0acb3ebfcbcf6201735a3263937e5
KiUkKim <zidh1@korea.ac.kr>
2022-09-17 17:44:53 +0900

    CK_GA-5 Ver3 API - Cup Rental & Return // 컵의 상태에 따른 가능 여부 메시지 반환

--- src/main/java/com/ck/reusable/springboot/domain/user/CupRepository.java
+++ src/main/java/com/ck/reusable/springboot/domain/user/CupRepository.java
@@ -1,6 +1,18 @@
 package com.ck.reusable.springboot.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CupRepository extends JpaRepository<Cup, Long> {
+
+    /*
+    Cup State Check Repository
+     */
+//    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Cup c WHERE c.cupUid = :cupUid")
+//    boolean CanRental(@Param("cupUid") Long cupUid);
+
+    @Query("SELECT c.cupState From Cup c WHERE c.cupUid = :cupUid")
+    Integer CanRental(@Param("cupUid") Long cupUid);
+
 }
--- src/main/java/com/ck/reusable/springboot/web/QrApiController.java
+++ src/main/java/com/ck/reusable/springboot/web/QrApiController.java
@@ -1,12 +1,10 @@
 package com.ck.reusable.springboot.web;
 
+import com.ck.reusable.springboot.service.Qr.QrService;
 import com.ck.reusable.springboot.service.user.UserService;
 import com.ck.reusable.springboot.web.dto.QrDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -16,15 +14,39 @@ public class QrApiController {
 
     private final UserService userService;
 
+    private final QrService qrService;
 
 
-//    @PutMapping("/manager/Qrcheck")
-//    @ResponseBody
-//    public String CupStateCheck(QrDto.ForQrResponseDto qrResponseDto, Principal principal)
-//    {
-//        Long cupUid = qrResponseDto.getCup_uid();
-//        String userEmail = principal.getName();
-//
-//
-//    }
+    @PutMapping("/manager/Qrcheck")
+    @ResponseBody
+    public String CupStateCheck(@RequestBody QrDto.ForQrResponseDto qrResponseDto, Principal principal)
+    {
+        Long cupUid = qrResponseDto.getCup_uid();
+
+        System.out.println("cupUid - " + cupUid);
+        String userEmail = principal.getName();
+
+        Integer check = qrService.checkCupStateService(cupUid);
+        System.out.println("check - "  + check);
+        String message = "";
+        /*
+        cupState 에 따른 구분 ( 0 : 대여가능, 1 : 대여중, 2: 반납 , 3: 세척 )
+         */
+        switch (check){
+            case 0:
+                message = "대여 가능한 컵입니다.";
+                break;
+            case 1:
+                message = "대여가 불가능합니다. [ 대여중인 컵  ] ";
+                break;
+            case 2:
+                message = "대여가 불가능합니다. [ 반납처리 된 컵] ";
+                break;
+            case 3:
+                message = "대여가 불가능합니다. [ 세척 중인 컵 ] ";
+                break;
+        }
+
+        return message;
+    }
 }
